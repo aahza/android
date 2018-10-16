1673b8e05c0278b1f6c76727596635ad5dc97a10
alexander.zaleski <alexander.zaleski@promwad.com>
2018-10-16 20:29:36 +0300

    fixed names drawable resources

--- MVCApp/app/src/main/java/test/aahz/com/mvcapp/MainActivity.java
+++ MVCApp/app/src/main/java/test/aahz/com/mvcapp/MainActivity.java
@@ -2,6 +2,17 @@ package test.aahz.com.mvcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner sp = (Spinner) findViewById(R.id.spinner);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        final List<String> planets = new ArrayList<>(asList("Mercury", "Venus", "Earth",
+                "Mars", "Jupiter", "Saturn", "Uranus ", "Neptune", "X-planet"));
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, planets);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+//        sp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(getApplicationContext(), "Planet #" + position, Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
