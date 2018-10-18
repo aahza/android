99a3f8b56a2a02a311f9b16b87c7250fdd92538a
alexander.zaleski <alexander.zaleski@promwad.com>
2018-10-18 14:02:15 +0300

    added Intent(MediaStore.ACTION_IMAGE_CAPTURE)

--- IntentApp/app/src/main/java/test/aahz/com/intentapp/MainActivity.java
+++ IntentApp/app/src/main/java/test/aahz/com/intentapp/MainActivity.java
@@ -1,12 +1,22 @@
 package test.aahz.com.intentapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +25,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        Intent intent;
         switch (view.getId()) {
             case R.id.button1:
                 startActivity(new Intent(this, SecondActivity.class));
                 break;
             case R.id.button2:
 //                Toast.makeText(getApplicationContext(), "No action attached", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(this, SecondActivity.class);
-                intent.putExtra("string", "I m from \n" + this.getClass().getName() + " & local name:\n" + getLocalClassName());
+                intent = new Intent(this, SecondActivity.class);
+                intent.putExtra("string", "I m from \n" + this.getClass().getName());
+
+                // image as byte stream
+//                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
+//                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//                bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+//                byte[] bImage = byteArrayOutputStream.toByteArray();
+//                intent.putExtra("byte_image", bImage);
+
+                intent.putExtra("image", R.drawable.cat0);
                 startActivity(intent);
+                break;
+            case R.id.button3:
+//                Toast.makeText(getApplicationContext(), "No action attached", Toast.LENGTH_SHORT).show();
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView imvMain = findViewById(R.id.imageMain);
+        switch (requestCode) {
+            case CAMERA:
+                Bitmap cameraImg = (Bitmap) data.getExtras().get("data");
+                imvMain.setImageBitmap(cameraImg);
                 break;
         }
     }
--- IntentApp/app/src/main/java/test/aahz/com/intentapp/SecondActivity.java
+++ IntentApp/app/src/main/java/test/aahz/com/intentapp/SecondActivity.java
@@ -1,13 +1,17 @@
 package test.aahz.com.intentapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity  extends AppCompatActivity {
 
     TextView tv2;
+    ImageView imv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,6 +19,21 @@ public class SecondActivity  extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         tv2 = (TextView) findViewById(R.id.txtV2);
-        tv2.setText(getIntent().getStringExtra("string"));
+        tv2.setText("Cat by default");
+
+        try{
+            String extra = getIntent().getStringExtra("string");
+            if(extra != null|| (! extra.isEmpty()))
+                tv2.setText(extra);
+        } catch (Exception e) {e.printStackTrace();}
+
+        imv = (ImageView) findViewById(R.id.imageView);
+
+        imv.setImageResource(getIntent().getIntExtra("image", R.drawable.cat));
+
+        //from bitmap
+//        byte[] bImage = getIntent().getByteArrayExtra("byte_image");
+//        Bitmap bitmap = BitmapFactory.decodeByteArray(bImage, 0, bImage.length);
+//        imv.setImageBitmap(bitmap);
     }
 }
