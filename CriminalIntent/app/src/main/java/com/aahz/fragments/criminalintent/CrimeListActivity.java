2027037d62b8e7d1c2b1598f3c8f149217b770cd
alexander.zaleski <alexander.zaleski@promwad.com>
2018-12-04 20:56:51 +0300

    recyclerview implementation for list manager

---
+++ CriminalIntent/app/src/main/java/com/aahz/fragments/criminalintent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package com.aahz.fragments.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
