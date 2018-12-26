a745f0a3bb5d3d7b68c816cfc05481951f3a8d7e
alexander.zaleski <alexander.zaleski@promwad.com>
2018-12-26 21:13:31 +0300

    SoundPool play wav from assets

--- BeatBoxApp/app/src/main/java/com/aahz/beatboxapp/Sound.java
+++ BeatBoxApp/app/src/main/java/com/aahz/beatboxapp/Sound.java
@@ -1,8 +1,12 @@
 package com.aahz.beatboxapp;
 
+
 public class Sound {
     private String mAssetPath;
     private String mName;
+
+
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -18,4 +22,13 @@ public class Sound {
     public String getName() {
         return mName;
     }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+
 }
